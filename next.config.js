5a0cef825ae6b99b2d40cf04977f139689229893
ᴀʙᴅᴜʟʟᴀʜ ᴀʟ ɴᴏᴍᴀɴ ᴘʀɪɴᴄᴇ <prince1000bd@gmail.com>
2023-08-23 20:02:31 +0600

    note.txt file added

--- next.config.js
+++ next.config.js
@@ -1,11 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    distDir: 'build',
-    async headers(){
-        return [
-            {source:"/:path*", headers:[{key:'X-Frame-Options', value: 'DENY'}]}
-        ]
-    }
-}
+  distDir: "build",
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [{ key: "X-Frame-Options", value: "DENY" }],
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
